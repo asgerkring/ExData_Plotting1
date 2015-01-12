ff593b09427f7d2b445aa443dd7c92f91aa6302e
Asger Kring <git@kring.dk>
2015-01-12 01:12:33 +0100

    Added the 4 R scripts and the 4 plots required in this assignment

---
+++ plot1.R
@@ -0,0 +1,38 @@
+# Assignment 1, Exploratory Data Analisys
+# Step 1 - read the data
+# all_rows <- read.table("data/household_power_consumption.txt", header=TRUE, nrows=9)
+
+# Step 2 - strip irrelevenant dates
+# power_consumption <- subset(all_rows, Date=="01/")
+# The data is read using fread from the data.table package. Only the lines containing the
+# dates we are interested in are read.
+#
+# For dates and months < 10, there are not leading zeros. Thus, the selection is on the
+# dates 1/2/2007 and 2/2/2007, as opposed to 01/02/2007 and 02/02/2007
+#
+# We will use fread so data.table package must be loaded
+
+powerdata <- "data/household_power_consumption.txt"
+PD <- fread(paste("grep \"^[12]/2/2007\"", powerdata), na.strings = "?")
+# Headers a "grepped out", and must be restored
+
+names <- colnames(fread(powerdata, nrows=0))
+
+setnames(PD, names)
+
+# Add a column with combined date/time information
+PDT <- transform(PD, DateTime = strptime(paste(PD$Date, PD$Time), format="%d/%m/%Y %H:%M:%S", tz="EST"))
+
+# We'll only have a single graph here
+par(mfrow = c(1,1))
+# Plot the graph with the relevant labels, titel and color:
+hist(
+  as.numeric(PDT$Global_active_power), 
+     main = paste ("Global Active Power"), 
+     xlab="Global Active Power (kilowatts)", 
+     ylab="Frequency", 
+     col="red"
+  )
+# Output to file
+dev.copy(png, file="plot1.png")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,44 @@
+# Assignment 1, Exploratory Data Analisys
+# Step 1 - read the data
+# all_rows <- read.table("data/household_power_consumption.txt", header=TRUE, nrows=9)
+
+# Step 2 - strip irrelevenant dates
+# power_consumption <- subset(all_rows, Date=="01/")
+# The data is read using fread from the data.table package. Only the lines containing the
+# dates we are interested in are read.
+#
+# For dates and months < 10, there are not leading zeros. Thus, the selection is on the
+# dates 1/2/2007 and 2/2/2007, as opposed to 01/02/2007 and 02/02/2007
+#
+# We will use fread so data.table package must be loaded
+
+powerdata <- "data/household_power_consumption.txt"
+PD <- fread(paste("grep \"^[12]/2/2007\"", powerdata), na.strings = "?")
+# Headers a "grepped out", and must be restored
+
+names <- colnames(fread(powerdata, nrows=0))
+
+setnames(PD, names)
+
+# Add a column with combined date/time information
+PDT <- transform(PD, DateTime = strptime(paste(PD$Date, PD$Time), format="%d/%m/%Y %H:%M:%S", tz="EST"))
+
+# We'll only have a single graph here
+par(mfrow = c(1,1))
+# Plot the first submeter data - black.
+plot(
+  x=PDT$DateTime,
+  y=as.numeric(PDT$Sub_metering_1),
+  type="l",
+  ylab="Energy sub metering",
+  xlab=""
+  )
+# Then add the next datapoints - red and blue
+lines(x=PDT$DateTime, y=as.numeric(PDT$Sub_metering_2), col="red")
+lines(x=PDT$DateTime, y=as.numeric(PDT$Sub_metering_3), col="blue")
+# And finally, the legend
+legend("topright", lty=1, col=c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# Output to file
+dev.copy(png, file="plot3.png")
+dev.off()
